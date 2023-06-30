015be7b253b6bffbde8a4e0f8147364b268f6eba
Patrick Mendoza <patmendoza6745@berkeley.edu>
2023-06-30 14:16:35 -0400

    Update vehicle_test.py

--- approach_1c/vehicle_test.py
+++ approach_1c/vehicle_test.py
@@ -4,5 +4,5 @@ from vehicle import *
 A file that will consist of unit tests for the functionality of the trajectory method of a vehicle. For e.g.,
 make sure the acceleration phase, random phase, and deceleration phase all work properly.
 """
-acceleration = Vehicle(70, 4, "corolla", "toyota").trajectory(1, 5, 100, 2)
+acceleration = Vehicle(70, 4, "corolla", "toyota").trajectory(1, 5, 100, 1)
 print(acceleration)
