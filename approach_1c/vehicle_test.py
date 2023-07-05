3382ec00d33961b7d90851903791a470808216ad
Patrick Mendoza <patmendoza6745@berkeley.edu>
2023-07-05 00:33:40 -0400

    Add simple test/demos

--- approach_1c/vehicle_test.py
+++ approach_1c/vehicle_test.py
@@ -4,5 +4,4 @@ from vehicle import *
 A file that will consist of unit tests for the functionality of the trajectory method of a vehicle. For e.g.,
 make sure the acceleration phase, random phase, and deceleration phase all work properly.
 """
-acceleration = Vehicle(70, 4, "corolla", "toyota").trajectory(1, 5, 100, 1)
-print(acceleration)
+acceleration2 = Vehicle(70, 4, "corolla", "toyota").trajectory(1, 1, 100, 4).report()
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from approach_1c import vehicle
+from format import *
 
-
-
+corolla = vehicle.Vehicle(70, 4, "corolla", "toyota")
+corolla.trajectory(1, 1, 100, 1)
+traj = corolla.report()
+print(traj)
+plot_trajectory_1D(traj)
