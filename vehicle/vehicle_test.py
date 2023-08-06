bc49c137f35fce01847e9e4ce095b6df26ad3f3f
Patrick Mendoza <patmendoza6745@berkeley.edu>
2023-08-06 15:42:53 -0400

    Delete vehicle_test.py

--- vehicle/vehicle_test.py
+++
@@ -1,9 +0,0 @@
-from vehicle import *
-
-"""
-A file that will consist of unit tests for the functionality of the trajectory method of a vehicle. For e.g.,
-make sure the acceleration phase, random phase, and deceleration phase all work properly.
-"""
-acceleration2 = Vehicle(70, 4, "corolla", "toyota")
-acceleration2.trajectory(1, 1, 100, 4)
-
