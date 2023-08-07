5564c205aceda8b7bb296da8c0362b7e53d56aac
Patrick Mendoza <patmendoza6745@berkeley.edu>
2023-08-07 19:34:23 -0400

    Update main to show differing trajectories

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from vehicle.attack import *
 from format import *
 
 attk = Attack(70, 4)
-traj = attk.benign(1, 1, 500, 10)
-plot_trajectory_1D(traj)
+faulty, benign = attk.compare(1, 1, 500, 10, scenario=2)
+
+plot_trajectory_1D(faulty)
+plot_trajectory_1D(benign)
