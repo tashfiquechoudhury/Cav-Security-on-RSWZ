e38f5d069fcb6995e0e42a38a7043610d90f4ed0
Patrick Mendoza <patmendoza6745@berkeley.edu>
2023-08-08 17:11:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vehicle.attack import *
 from format import *
 
 attk = Attack(70, 4)
-faulty, benign = attk.compare(1, 1, 500, 10, scenario=2)
+faulty, benign = attk.compare(1, 1, 500, 10, scenario=1)
 
 plot_trajectory_1D(faulty)
 plot_trajectory_1D(benign)
